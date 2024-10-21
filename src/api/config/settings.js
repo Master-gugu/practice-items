73220d6cc2545335f2b3cf06e63e3f0b5ee1b320
master-gugu <2694692439@qq.com>
2024-10-21 18:40:49 +0800

    提交

--- src/api/config/settings.js
+++ src/api/config/settings.js
@@ -1,6 +1,8 @@
 var config = {
-    "base_url": "http://uyuzvw.natappfree.cc",
-    "ws_url": "ws://uyuzvw.natappfree.cc",
+    // "base_url": "http://th57g8.natappfree.cc",
+    "base_url": "https://192.168.2.33:9090",
+    // "ws_url": "ws://th57g8.natappfree.cc",
+    "ws_url": "ws://192.168.2.33:9090",
 }
 
 try {
--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = defineConfig({
     open: false, // 启动服务后打开浏览器
     host: '0.0.0.0', // 指定使用IP地址
     port: 6060, // 设置端口号
-    https: false, // 使用http协议
+    https: true, // 使用http协议
     proxy: {
       '/api': {
         target: config.base_url, // 代理地址
